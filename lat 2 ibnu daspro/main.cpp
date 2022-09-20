166cb91f98fdca4e355bc4541e37e8f7c15cf82f
IbnuFaridd <113011904+IbnuFaridd@users.noreply.github.com>
2022-09-20 09:56:43 +0700

    Add files via upload

---
+++ lat 2 ibnu daspro/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "BIODATAKU" << endl;
+    /*printf("teks/n");
+    nama : ibnu farid mabina
+    NIM :A11202214591
+    */
+
+    int u = 18; //deklarasi
+    float t = 100; //inisiasi
+    char e = 'l'; //untuk 1 huruf
+    string w = "xl"; //untuk lebih dari 1 huruf
+
+        cout << "umur aku : " << u;
+        cout << endl;
+        cout << "ukuran celana : " << t;
+        cout << "ukuran baju : " << w;
+        cout << endl;
+
+        int a;
+        cout << "masukkan angka: ";
+        cin >> a;
+        cout << "angka anda + 18: " << a + u;
+
+    return 0;
+}
